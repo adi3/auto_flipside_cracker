3cfe8030e3edb95e1968a7ce9c43cdd64f7abdd4
Adi Singh <adisin8@gmail.com>
2014-02-22 17:04:51 -0800

    Added code for automatic navigation.

--- src/background.js
+++ src/background.js
@@ -1,24 +1,42 @@
+var tabId = null;
+
 function setAlarm() {
-    chrome.alarms.create('Midnight', {
-        periodInMinutes: 1
+    chrome.alarms.create('DailyCheck', {
+        periodInMinutes: 1440
+    });
+
+    chrome.alarms.getAll(function(alarms) {
+        console.log(alarms);
     });
 }
 
+function navigate() {
+    var page_url = "http://stanfordflipside.com/category/puzzles/";
+
+    chrome.tabs.create({
+        url: page_url,
+        active: false
+    }, function(tab) {
+        tabId = tab.id;
+    });
+}
+
+function close() {
+    chrome.tabs.remove(tabId);
+}
+
 chrome.runtime.onInstalled.addListener(function(details) {
-	setAlarm();
+    setAlarm();
 });
 
 chrome.runtime.onStartup.addListener(function(details) {
-	setAlarm();
+    setAlarm();
 });
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
-    var page_url = "http://stanfordflipside.com/category/puzzles/";
-
-    if (alarm.name === 'Midnight') {
-        chrome.tabs.create({
-            url: page_url,
-            active: false
-        });
+    if (alarm.name === 'DailyCheck') {
+        navigate();
+        console.log("Opened in tab " + tabId);
+        window.setInterval(close, 12000);
     }
 });
